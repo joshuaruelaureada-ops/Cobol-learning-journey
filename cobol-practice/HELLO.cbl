a457a477fcd147e1afb66d72c70df43675cc03e0
joshuaruelaureada-ops <joshuaruelaureada@gmail.com>
2026-07-27 16:18:17 +0800

    Create HELLO.cbl

---
+++ cobol-practice/HELLO.cbl
@@ -0,0 +1,12 @@
+IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+           DISPLAY "==========================================".
+           DISPLAY "  HELLO WORLD! WELCOME TO THE MAINFRAME.  ".
+           DISPLAY "==========================================".
+           STOP RUN.
